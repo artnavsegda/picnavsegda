fb94e26499d8ff2ab60ca03a7b8daec8cab29720
Art Navsegda <artnavsegda@gmail.com>
2017-03-15 10:24:16 +0300

    ethernet

---
+++ mplabx/mcc/pic18f97j60/ethernet.X/mcc_generated_files/tmr1.c
@@ -0,0 +1,182 @@
+/**
+  TMR1 Generated Driver File
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    tmr1.c
+
+  @Summary
+    This is the generated driver implementation file for the TMR1 driver using MPLAB(c) Code Configurator
+
+  @Description
+    This source file provides APIs for TMR1.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - 4.15
+        Device            :  PIC18F97J60
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 1.35
+        MPLAB             :  MPLAB X 3.40
+*/
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+#include "tmr1.h"
+
+/**
+  Section: Global Variable Definitions
+*/
+volatile uint16_t timer1ReloadVal;
+void (*TMR1_InterruptHandler)(void);
+/**
+  Section: TMR1 APIs
+*/
+
+void TMR1_Initialize(void)
+{
+    //Set the Timer to the options selected in the GUI
+
+    //T1CKPS 1:4; T1OSCEN disabled; TMR1CS External; nT1SYNC synchronize; TMR1ON off; RD16 disabled; 
+    T1CON = 0x22;
+
+    //TMR1H 224; 
+    TMR1H = 0xE0;
+
+    //TMR1L 0; 
+    TMR1L = 0x00;
+
+    // Load the TMR value to reload variable
+    timer1ReloadVal=TMR1;
+
+    // Clearing IF flag before enabling the interrupt.
+    PIR1bits.TMR1IF = 0;
+
+    // Enabling TMR1 interrupt.
+    PIE1bits.TMR1IE = 1;
+
+    // Set Default Interrupt Handler
+    TMR1_SetInterruptHandler(TMR1_DefaultInterruptHandler);
+
+    // Start TMR1
+    TMR1_StartTimer();
+}
+
+void TMR1_StartTimer(void)
+{
+    // Start the Timer by writing to TMRxON bit
+    T1CONbits.TMR1ON = 1;
+}
+
+void TMR1_StopTimer(void)
+{
+    // Stop the Timer by writing to TMRxON bit
+    T1CONbits.TMR1ON = 0;
+}
+
+uint16_t TMR1_ReadTimer(void)
+{
+    uint16_t readVal;
+    uint8_t readValHigh;
+    uint8_t readValLow;
+    
+    readValLow = TMR1L;
+    readValHigh = TMR1H;
+    
+    readVal = ((uint16_t)readValHigh << 8) | readValLow;
+
+    return readVal;
+}
+
+void TMR1_WriteTimer(uint16_t timerVal)
+{
+    if (T1CONbits.nT1SYNC == 1)
+    {
+        // Stop the Timer by writing to TMRxON bit
+        T1CONbits.TMR1ON = 0;
+
+        // Write to the Timer1 register
+        TMR1H = (timerVal >> 8);
+        TMR1L = (uint8_t) timerVal;
+
+        // Start the Timer after writing to the register
+        T1CONbits.TMR1ON =1;
+    }
+    else
+    {
+        // Write to the Timer1 register
+        TMR1H = (timerVal >> 8);
+        TMR1L = (uint8_t) timerVal;
+    }
+}
+
+void TMR1_Reload(void)
+{
+    // Write to the Timer1 register
+    TMR1H = (timer1ReloadVal >> 8);
+    TMR1L = (uint8_t) timer1ReloadVal;
+}
+
+void TMR1_ISR(void)
+{
+
+    // Clear the TMR1 interrupt flag
+    PIR1bits.TMR1IF = 0;
+    
+     // Write to the Timer1 register
+     TMR1H = (timer1ReloadVal >> 8);
+     TMR1L =  (uint8_t) timer1ReloadVal;
+
+    // ticker function call;
+    // ticker is 1 -> Callback function gets called every time this ISR executes
+    TMR1_CallBack();
+}
+
+void TMR1_CallBack(void)
+{
+    // Add your custom callback code here
+
+    if(TMR1_InterruptHandler)
+    {
+        TMR1_InterruptHandler();
+    }
+}
+
+void TMR1_SetInterruptHandler(void* InterruptHandler){
+    TMR1_InterruptHandler = InterruptHandler;
+}
+
+void TMR1_DefaultInterruptHandler(void){
+    // add your TMR1 interrupt custom code
+    // or set custom function using TMR1_SetInterruptHandler()
+}
+
+/**
+ End of File
+*/
