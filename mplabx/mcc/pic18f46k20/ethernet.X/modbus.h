a41176ab512e35591e5bb6e1a2ef546097d012eb
Art Navsegda <artnavsegda@gmail.com>
2017-03-24 05:02:26 +0300

    ochko zhim zhim

---
+++ mplabx/mcc/pic18f46k20/ethernet.X/modbus.h
@@ -0,0 +1,57 @@
+typedef struct  {
+        unsigned int firstreg;
+        unsigned int regnumber;
+}askreadregstruct;
+
+typedef struct  {
+        unsigned char bytestofollow;
+        unsigned char coils[256];
+}reqreadcoilsstruct;
+
+typedef struct  {
+        unsigned char bytestofollow;
+        unsigned int registers[128];
+}reqreadwordstruct;
+
+typedef struct  {
+        unsigned int regaddress;
+        unsigned int regvalue;
+}writeregstruct;
+
+typedef struct  {
+        unsigned int firstreg;
+        unsigned int regnumber;
+        unsigned char bytestofollow;
+        unsigned char coils[256];
+}writemulticoilstruct;
+
+typedef struct  {
+        unsigned int firstreg;
+        unsigned int regnumber;
+        unsigned char bytestofollow;
+        unsigned int registers[128];
+}writemultiregstruct;
+
+typedef union  {
+        askreadregstruct askreadregs;
+        reqreadcoilsstruct reqreadcoils;
+        reqreadwordstruct reqreadholdings;
+        writeregstruct writereg;
+        writemulticoilstruct writemulticoil;
+        writemultiregstruct writemultireg;
+        unsigned int words[128];
+        unsigned char bytes[256];
+}pdudataunion;
+
+typedef struct  {
+        unsigned char unitid;
+        unsigned char fncode;
+        pdudataunion values;
+}pduframestruct;
+
+typedef struct  {
+        unsigned int tsid;
+        unsigned int protoid;
+        unsigned int length;
+        pduframestruct pdu;
+}mbframestruct;
