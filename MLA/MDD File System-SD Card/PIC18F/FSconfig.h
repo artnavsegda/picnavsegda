332014f1b3b71d78d53d58b99e774d98875ebefb
Art Navsegda <artnavsegda@gmail.com>
2017-03-25 14:45:12 +0300

    memory

--- MLA/MDD File System-SD Card/PIC18F/FSconfig.h
+++ MLA/MDD File System-SD Card/PIC18F/FSconfig.h
@@ -13,8 +13,8 @@
  * Software License Agreement
  *
  * The software supplied herewith by Microchip Technology Incorporated
- * (the “Company”) for its PICmicro® Microcontroller is intended and
- * supplied to you, the Company’s customer, for use solely and
+ * (the ï¿½Companyï¿½) for its PICmicroï¿½ Microcontroller is intended and
+ * supplied to you, the Companyï¿½s customer, for use solely and
  * exclusively on Microchip PICmicro Microcontroller products. The
  * software is owned by the Company and/or its supplier, and is
  * protected under applicable copyright laws. All rights are reserved.
@@ -23,7 +23,7 @@
  * civil liability for the breach of the terms and conditions of this
  * license.
  *
- * THIS SOFTWARE IS PROVIDED IN AN “AS IS” CONDITION. NO WARRANTIES,
+ * THIS SOFTWARE IS PROVIDED IN AN ï¿½AS ISï¿½ CONDITION. NO WARRANTIES,
  * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
  * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
@@ -78,7 +78,7 @@
 // Description: The ALLOW_DIRS definition can be commented out to disable all directory functionality.  This will reduce code size.
 //              If directories are enabled, write operations must also be enabled by uncommenting ALLOW_WRITES in order to use
 //              the FSmkdir or FSrmdir functions.
-#define ALLOW_DIRS
+//#define ALLOW_DIRS
 
 // Summary: A macro to enable/disable PIC18 ROM functions.
 // Description: The ALLOW_PGMFUNCTIONS definition can be commented out to disable all PIC18 functions that allow the user to pass string
