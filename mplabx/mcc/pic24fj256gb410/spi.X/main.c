424c8450e34c5eeb9d17bf220ea1f837a695e61d
Art Navsegda <artnavsegda@gmail.com>
2017-11-17 02:21:59 +0300

    spi ads1256 transfer

---
+++ mplabx/mcc/pic24fj256gb410/spi.X/main.c
@@ -0,0 +1,99 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using MPLAB(c) Code Configurator
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - pic24-dspic-pic32mm : v1.26
+        Device            :  PIC24FJ128GB410
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.30
+        MPLAB             :  MPLAB X 3.45
+*/
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include <stdio.h>
+
+/*
+                         Main application
+ */
+
+void ads1256config()
+{
+	while(_RD1);
+	_LATD2 = 0;
+	SPI1_Exchange8bit(0x50|0);
+	SPI1_Exchange8bit(0x03);
+	SPI1_Exchange8bit(0x00);
+	SPI1_Exchange8bit(0x08);
+	SPI1_Exchange8bit(0x00);
+	SPI1_Exchange8bit(0x03);
+	_LATD2 = 1;
+}
+
+int main(void)
+{
+    uint32_t result = 0;
+    // initialize the device
+    SYSTEM_Initialize();
+    
+    while(_RD1);
+    _LATD2 = 0;
+    SPI1_Exchange8bit(0x10|0);
+    SPI1_Exchange8bit(0x0);
+    uint8_t _data = SPI1_Exchange8bit(0xFF);
+    _LATD2 = 1;
+    printf("chip id %d\n\r",_data >> 4);
+    
+    ads1256config();
+
+    while (1)
+    {
+        if (_RD1 == 0)
+        {
+            _LATD2 = 0;
+            SPI1_Exchange8bit(0x01);
+			((char *)&result)[2] = SPI1_Exchange8bit(0xFF);
+			((char *)&result)[1] = SPI1_Exchange8bit(0xFF);
+            ((char *)&result)[0] = SPI1_Exchange8bit(0xFF);
+            _LATD2 = 1;
+            printf("%lu\r\n",result);
+        }
+    }
+
+    return -1;
+}
+/**
+ End of File
+*/
---
+++ mplabx/mcc/pic24fj256gb410/spi.X/mcc_generated_files/pin_manager.h
@@ -0,0 +1,529 @@
+/**
+  System Interrupts Generated Driver File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the generated manager file for the MPLAB(c) Code Configurator device.  This manager
+    configures the pins direction, initial state, analog setting.
+    The peripheral pin select, PPS, configuration is also handled by this manager.
+
+  @Description:
+    This source file provides implementations for MPLAB(c) Code Configurator interrupts.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - 4.15.1
+        Device            :  PIC24FJ128GB410
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.30
+        MPLAB             :  MPLAB X 3.45
+
+    Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+    Microchip licenses to you the right to use, modify, copy and distribute
+    Software only when embedded on a Microchip microcontroller or digital signal
+    controller that is integrated into your product or third party product
+    (pursuant to the sublicense terms in the accompanying license agreement).
+
+    You should refer to the license agreement accompanying this Software for
+    additional information regarding your rights and obligations.
+
+    SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+    EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+    MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+    IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+    CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+    OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+    INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+    CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+    SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+    (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+*/
+
+#ifndef _PIN_MANAGER_H
+#define _PIN_MANAGER_H
+/**
+    Section: Includes
+*/
+#include <xc.h>
+/**
+    Section: Device Pin Macros
+*/
+/**
+  @Summary
+    Sets the GPIO pin, RD1, high using LATD1.
+
+  @Description
+    Sets the GPIO pin, RD1, high using LATD1.
+
+  @Preconditions
+    The RD1 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RD1 high (1)
+    DRDY_ADS1256_SetHigh();
+    </code>
+
+*/
+#define DRDY_ADS1256_SetHigh()          _LATD1 = 1
+/**
+  @Summary
+    Sets the GPIO pin, RD1, low using LATD1.
+
+  @Description
+    Sets the GPIO pin, RD1, low using LATD1.
+
+  @Preconditions
+    The RD1 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RD1 low (0)
+    DRDY_ADS1256_SetLow();
+    </code>
+
+*/
+#define DRDY_ADS1256_SetLow()           _LATD1 = 0
+/**
+  @Summary
+    Toggles the GPIO pin, RD1, using LATD1.
+
+  @Description
+    Toggles the GPIO pin, RD1, using LATD1.
+
+  @Preconditions
+    The RD1 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Toggle RD1
+    DRDY_ADS1256_Toggle();
+    </code>
+
+*/
+#define DRDY_ADS1256_Toggle()           _LATD1 ^= 1
+/**
+  @Summary
+    Reads the value of the GPIO pin, RD1.
+
+  @Description
+    Reads the value of the GPIO pin, RD1.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    uint16_t portValue;
+
+    // Read RD1
+    postValue = DRDY_ADS1256_GetValue();
+    </code>
+
+*/
+#define DRDY_ADS1256_GetValue()         _RD1
+/**
+  @Summary
+    Configures the GPIO pin, RD1, as an input.
+
+  @Description
+    Configures the GPIO pin, RD1, as an input.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RD1 as an input
+    DRDY_ADS1256_SetDigitalInput();
+    </code>
+
+*/
+#define DRDY_ADS1256_SetDigitalInput()  _TRISD1 = 1
+/**
+  @Summary
+    Configures the GPIO pin, RD1, as an output.
+
+  @Description
+    Configures the GPIO pin, RD1, as an output.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RD1 as an output
+    DRDY_ADS1256_SetDigitalOutput();
+    </code>
+
+*/
+#define DRDY_ADS1256_SetDigitalOutput() _TRISD1 = 0
+/**
+  @Summary
+    Sets the GPIO pin, RE2, high using LATE2.
+
+  @Description
+    Sets the GPIO pin, RE2, high using LATE2.
+
+  @Preconditions
+    The RE2 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RE2 high (1)
+    PELTIER_ENABLE_SetHigh();
+    </code>
+
+*/
+#define PELTIER_ENABLE_SetHigh()          _LATE2 = 1
+/**
+  @Summary
+    Sets the GPIO pin, RE2, low using LATE2.
+
+  @Description
+    Sets the GPIO pin, RE2, low using LATE2.
+
+  @Preconditions
+    The RE2 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RE2 low (0)
+    PELTIER_ENABLE_SetLow();
+    </code>
+
+*/
+#define PELTIER_ENABLE_SetLow()           _LATE2 = 0
+/**
+  @Summary
+    Toggles the GPIO pin, RE2, using LATE2.
+
+  @Description
+    Toggles the GPIO pin, RE2, using LATE2.
+
+  @Preconditions
+    The RE2 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Toggle RE2
+    PELTIER_ENABLE_Toggle();
+    </code>
+
+*/
+#define PELTIER_ENABLE_Toggle()           _LATE2 ^= 1
+/**
+  @Summary
+    Reads the value of the GPIO pin, RE2.
+
+  @Description
+    Reads the value of the GPIO pin, RE2.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    uint16_t portValue;
+
+    // Read RE2
+    postValue = PELTIER_ENABLE_GetValue();
+    </code>
+
+*/
+#define PELTIER_ENABLE_GetValue()         _RE2
+/**
+  @Summary
+    Configures the GPIO pin, RE2, as an input.
+
+  @Description
+    Configures the GPIO pin, RE2, as an input.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RE2 as an input
+    PELTIER_ENABLE_SetDigitalInput();
+    </code>
+
+*/
+#define PELTIER_ENABLE_SetDigitalInput()  _TRISE2 = 1
+/**
+  @Summary
+    Configures the GPIO pin, RE2, as an output.
+
+  @Description
+    Configures the GPIO pin, RE2, as an output.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RE2 as an output
+    PELTIER_ENABLE_SetDigitalOutput();
+    </code>
+
+*/
+#define PELTIER_ENABLE_SetDigitalOutput() _TRISE2 = 0
+/**
+  @Summary
+    Sets the GPIO pin, RE3, high using LATE3.
+
+  @Description
+    Sets the GPIO pin, RE3, high using LATE3.
+
+  @Preconditions
+    The RE3 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RE3 high (1)
+    LEMP_ENABLE_SetHigh();
+    </code>
+
+*/
+#define LEMP_ENABLE_SetHigh()          _LATE3 = 1
+/**
+  @Summary
+    Sets the GPIO pin, RE3, low using LATE3.
+
+  @Description
+    Sets the GPIO pin, RE3, low using LATE3.
+
+  @Preconditions
+    The RE3 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RE3 low (0)
+    LEMP_ENABLE_SetLow();
+    </code>
+
+*/
+#define LEMP_ENABLE_SetLow()           _LATE3 = 0
+/**
+  @Summary
+    Toggles the GPIO pin, RE3, using LATE3.
+
+  @Description
+    Toggles the GPIO pin, RE3, using LATE3.
+
+  @Preconditions
+    The RE3 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Toggle RE3
+    LEMP_ENABLE_Toggle();
+    </code>
+
+*/
+#define LEMP_ENABLE_Toggle()           _LATE3 ^= 1
+/**
+  @Summary
+    Reads the value of the GPIO pin, RE3.
+
+  @Description
+    Reads the value of the GPIO pin, RE3.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    uint16_t portValue;
+
+    // Read RE3
+    postValue = LEMP_ENABLE_GetValue();
+    </code>
+
+*/
+#define LEMP_ENABLE_GetValue()         _RE3
+/**
+  @Summary
+    Configures the GPIO pin, RE3, as an input.
+
+  @Description
+    Configures the GPIO pin, RE3, as an input.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RE3 as an input
+    LEMP_ENABLE_SetDigitalInput();
+    </code>
+
+*/
+#define LEMP_ENABLE_SetDigitalInput()  _TRISE3 = 1
+/**
+  @Summary
+    Configures the GPIO pin, RE3, as an output.
+
+  @Description
+    Configures the GPIO pin, RE3, as an output.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RE3 as an output
+    LEMP_ENABLE_SetDigitalOutput();
+    </code>
+
+*/
+#define LEMP_ENABLE_SetDigitalOutput() _TRISE3 = 0
+
+/**
+    Section: Function Prototypes
+*/
+/**
+  @Summary
+    Configures the pin settings of the PIC24FJ128GB410
+    The peripheral pin select, PPS, configuration is also handled by this manager.
+
+  @Description
+    This is the generated manager file for the MPLAB(c) Code Configurator device.  This manager
+    configures the pins direction, initial state, analog setting.
+    The peripheral pin select, PPS, configuration is also handled by this manager.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    void SYSTEM_Initialize(void)
+    {
+        // Other initializers are called from this function
+        PIN_MANAGER_Initialize();
+    }
+    </code>
+
+*/
+void PIN_MANAGER_Initialize(void);
+
+#endif
