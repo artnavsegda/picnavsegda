721914f1567abe0b9b8dc56b901143f64efe5910
Art Navsegda <artnavsegda@gmail.com>
2017-11-18 23:59:51 +0300

    dac

---
+++ mplabx/mcc/pic24fj256gb410/dac.X/mcc_generated_files/dac1.c
@@ -0,0 +1,66 @@
+
+/**
+  DAC1 Generated Driver File 
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    dac1.c
+
+  @Summary
+    This is the generated driver implementation file for the DAC1 driver using MPLAB(c) Code Configurator
+
+  @Description
+    This header file provides implementations for driver APIs for DAC1. 
+    Generation Information : 
+        Product Revision  :  MPLAB(c) Code Configurator - pic24-dspic-pic32mm : v1.26
+        Device            :  PIC24FJ256GB410
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.30
+        MPLAB             :  MPLAB X 3.45
+*/
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+/**
+  Section: Included Files
+*/           
+#include "dac1.h"
+/**
+  Section: Driver Interface
+*/
+void DAC1_Initialize(void)
+{
+    // DACREF 2.4V internal band gap; DACOE enabled; DACFM Right; DACEN enabled; DACTSEL CMP1; DACTRIG disabled; DACSLP disabled; DACSIDL disabled; 
+    DAC1CON = 0x8083;
+}
+
+void DAC1_OutputSet(uint16_t inputData)
+{
+    DAC1DAT  = inputData;
+}
+
+/**
+  End of File
+*/ 
+
