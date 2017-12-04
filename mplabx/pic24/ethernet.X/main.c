c3b2bbdf42f1bd6a351cc6a2ec35788f3493cbaa
Art Navsegda <artnavsegda@gmail.com>
2017-12-04 08:19:47 +0300

    oukay

--- mplabx/pic24/ethernet.X/main.c
+++ mplabx/pic24/ethernet.X/main.c
@@ -1,9 +1,38 @@
 #define THIS_IS_STACK_APPLICATION
 #include "TCPIP Stack/TCPIP.h"
 
+void GenericTCPClient(void);
+void GenericTCPServer(void);
+void BerkeleyTCPClientDemo(void);
+void BerkeleyTCPServerDemo(void);
+void BerkeleyUDPClientDemo(void);
+
 APP_CONFIG AppConfig;
 BYTE AN0String[8];
 
-int main(void) {
+int main(void)
+{
+    StackInit();
+    
+    while(1)
+    {
+        StackTask();
+        StackApplications();
+        
+        #if defined(STACK_USE_GENERIC_TCP_CLIENT_EXAMPLE)
+        GenericTCPClient();
+        #endif
+
+        #if defined(STACK_USE_GENERIC_TCP_SERVER_EXAMPLE)
+        GenericTCPServer();
+        #endif
+
+        #if defined(STACK_USE_BERKELEY_API)
+        BerkeleyTCPClientDemo();
+        BerkeleyTCPServerDemo();
+        BerkeleyUDPClientDemo();
+        #endif
+    }
+    
     return 0;
 }
