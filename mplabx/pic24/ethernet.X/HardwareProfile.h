3cd301e29f2cbffa036355b3c0ddc87a011e57fa
Art Navsegda <artnavsegda@gmail.com>
2017-12-04 08:27:51 +0300

    madness

--- mplabx/pic24/ethernet.X/HardwareProfile.h
+++ mplabx/pic24/ethernet.X/HardwareProfile.h
@@ -53,9 +53,6 @@
 
 #include "Compiler.h"
 
-// Define a macro describing this hardware set up (used in other files)
-#define EXPLORER_16
-
 // Set configuration fuses (but only in MainDemo.c where THIS_IS_STACK_APPLICATION is defined)
 #if defined(THIS_IS_STACK_APPLICATION)
 	#if defined(__PIC24FJ256GB210__)
@@ -102,36 +99,6 @@
 
 
 // Hardware I/O pin mappings
-
-// LEDs
-#define LED0_TRIS			(TRISAbits.TRISA0)	// Ref D3
-#define LED0_IO				(LATAbits.LATA0)
-#define LED1_TRIS			(TRISAbits.TRISA1)	// Ref D4
-#define LED1_IO				(LATAbits.LATA1)
-#define LED2_TRIS			(TRISAbits.TRISA2)	// Ref D5
-#define LED2_IO				(LATAbits.LATA2)
-#define LED3_TRIS			(TRISAbits.TRISA3)	// Ref D6
-#define LED3_IO				(LATAbits.LATA3)
-#define LED4_TRIS			(TRISAbits.TRISA4)	// Ref D7
-#define LED4_IO				(LATAbits.LATA4)
-#define LED5_TRIS			(TRISAbits.TRISA5)	// Ref D8
-#define LED5_IO				(LATAbits.LATA5)
-#define LED6_TRIS			(TRISAbits.TRISA6)	// Ref D9
-#define LED6_IO				(LATAbits.LATA6)
-#define LED7_TRIS			(LATAbits.LATA7)	// Ref D10;  Note: This is multiplexed with BUTTON1, so this LED can't be used.  However, it will glow very dimmly due to a weak pull up resistor.
-#define LED7_IO				(LATAbits.LATA7)
-#define LED_GET()			(*((volatile unsigned char*)(&LATA)))
-#define LED_PUT(a)			(*((volatile unsigned char*)(&LATA)) = (a))
-
-// Momentary push buttons
-#define BUTTON0_TRIS		(TRISDbits.TRISD13)	// Ref S4
-#define	BUTTON0_IO			(PORTDbits.RD13)
-#define BUTTON1_TRIS		(TRISAbits.TRISA7)	// Ref S5;  Note: This is multiplexed with LED7
-#define	BUTTON1_IO			(PORTAbits.RA7)
-#define BUTTON2_TRIS		(TRISDbits.TRISD7)	// Ref S6
-#define	BUTTON2_IO			(PORTDbits.RD7)
-#define BUTTON3_TRIS		(TRISDbits.TRISD6)	// Ref S3
-#define	BUTTON3_IO			(PORTDbits.RD6)
 
 #define UARTTX_TRIS			(TRISFbits.TRISF5)
 #define UARTTX_IO			(PORTFbits.RF5)
