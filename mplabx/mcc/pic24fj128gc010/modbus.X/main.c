952d3bc33585a5db7bc7821aa9771613eafcbaf7
Art Navsegda <artnavsegda@gmail.com>
2017-12-14 07:37:36 +0300

    modbus

--- mplabx/mcc/pic24fj128gc010/modbus.X/main.c
+++ mplabx/mcc/pic24fj128gc010/modbus.X/main.c
@@ -46,31 +46,59 @@
 #include <stdio.h>
 #define FCY     (_XTAL_FREQ/2)
 #include <libpic30.h>
+#include "modbus.h"
+
+#define DEVICEID 50
+
+mbframestruct askmbframe, reqmbframe;
 
 /*
                          Main application
  */
+
+#define byteswap(toswap) asm("swap %0" : "+r"(toswap))
+
+int Swap(int number)
+{
+    byteswap(number);
+    return number;
+}
+
 int main(void)
 {
     uint8_t buffer[10];
-    int number, i;
+    int numread, i;
     
     // initialize the device
     SYSTEM_Initialize();
+    __delay_ms(100);
     printf("MCU started\r\n");
    
 
     while (1)
     {
-        number = UART2_ReadBuffer(buffer,10);
-        if (number > 0)
+        numread = UART2_ReadBuffer((uint8_t *)&askmbframe,10);
+        if (numread > 0)
         {
             //UART2_WriteBuffer(buffer,number);
-            printf("Recieved %d bytes to the buffer\r\n",number);
-            for (i=0; i<number;i++)
+            printf("Recieved %d bytes to the buffer\r\n",numread);
+            for (i=0; i<numread;i++)
                 printf("0x%02X ",buffer[i]);
             printf("\r\n");
-            UART2_WriteBuffer(buffer,number);
+            printf("Unit id: %u\r\n", askmbframe.unitid);
+            printf("Function code: %d\r\n", askmbframe.fncode);
+            for (i=0; i<((numread-2)/2)-1;i++)
+                printf("%u ",Swap(askmbframe.data.words[i]));
+            printf("\r\n");
+            printf("recieved CRC 0x%04X\n\r",askmbframe.data.words[((numread-2)/2)-1]);
+            
+            if (askmbframe.unitid == DEVICEID)
+            {
+                printf("device id matches\r\n");
+                UART2_WriteBuffer(buffer,numread);
+            }
+            else
+                printf("device id mismatch\n");
         }
         __delay_ms(100);
     }
