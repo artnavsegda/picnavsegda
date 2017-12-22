1a71c91c36215f31571a07cbde7f0b3cfae994c6
Art Navsegda <artnavsegda@gmail.com>
2017-12-22 16:49:46 +0300

    serial

--- mplabx/dspic33e/serial.X/main.c
+++ mplabx/dspic33e/serial.X/main.c
@@ -1,13 +1,8 @@
-/*
- * File:   main.c
- * Author: artnavsegda
- *
- * Created on December 22, 2017, 4:42 PM
- */
-
+#pragma config FNOSC = FRC              // Initial Oscillator Source Selection Bits (Internal Fast RC (FRC))
 
 #include "xc.h"
 
 int main(void) {
     return 0;
 }
+
